f0b580bd87341a504ac12b3759953eb210f8d4f4
Manvi <msinghal3102@gmail.com>
2021-07-21 18:38:07 +0530

    added heros and banners at all pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import { RoomProvider } from './context';
 
 ReactDOM.render(
+  <RoomProvider>
   <Router>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </Router>
+  </RoomProvider>,
+document.getElementById('root')
 );
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Banner from '../components/Banner'
+import {Link } from 'react-router-dom'
 import Hero from '../components/Hero'
+
 const Rooms = () => {
     return (
-        <Hero hero="roomsHero" />
+        <Hero hero="roomsHero">
+        <Banner title="our rooms">
+        <Link to="/rooms" className="btn-primary">
+        return home
+        </Link>
+        </Banner>
+        </Hero>
     )
 }
 export default Rooms;
