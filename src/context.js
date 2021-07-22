f015cf644a1a6c460d0091a2dd633346954cd4cb
Manvi <msinghal3102@gmail.com>
2021-07-22 14:47:10 +0530

    Added singleroom pages and alos more features

--- src/context.js
+++ src/context.js
@@ -12,6 +12,12 @@ class RoomProvider extends Component {
     componentDidMount(){
         let rooms=this.formatData(items)
         let featuredRooms = rooms.filter(room=>room.feature===true)
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        });
     }
     formatData(items){
         let tempItems = items.map(item=>{
@@ -22,9 +28,16 @@ class RoomProvider extends Component {
         });
         return tempItems;
     }
+    getRoom=slug=>{
+        let tempRooms = [...this.state.rooms];
+        const room = tempRooms.find(room=>room.slug===slug);
+        return room;
+    }
     render() {
         return (
-            <RoomContext.Provider value={{...this.state}}>
+            <RoomContext.Provider value={{...this.state,
+            getRoom: this.getRoom,
+            }}>
             {this.props.children}
             </RoomContext.Provider>
         )
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Banner from '../components/Banner'
 import {Link } from 'react-router-dom'
 import Hero from '../components/Hero'
+import RoomContainer from "../components/RoomContainer";
 
 const Rooms = () => {
     return (
+        <>
         <Hero hero="roomsHero">
         <Banner title="our rooms">
         <Link to="/rooms" className="btn-primary">
@@ -12,6 +14,8 @@ const Rooms = () => {
         </Link>
         </Banner>
         </Hero>
+        <RoomContainer/>
+        </>
     )
 }
 export default Rooms;
