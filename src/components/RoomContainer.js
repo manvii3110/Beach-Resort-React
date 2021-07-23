49e186d29271ef7191d0e174bff80bf114b2e442
Manvi <msinghal3102@gmail.com>
2021-07-23 15:53:31 +0530

    Added features for filtering rooms

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import RoomsFilter from "./RoomFilter";
-import RoomsList from "./RoomList";
-import {RoomConsumer} from "../context"
-import Loading from "./Loading"
+import React from "react";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
 
-export default function RoomContainer() {
-    return (
-        <RoomConsumer>
-        {value=>{
-            const {loading,sortedRooms,rooms} =value
-            if(loading){
-                return <Loading/>
-            }
-        return(
-        <div>
-            Hello from Room Container
-            <RoomsFilter rooms={rooms}/>
-            <RoomsList rooms={sortedRooms}/>
-            
-        </div>
-    );
-  }}
-    </RoomConsumer>
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
   );
 }
+
+export default withRoomConsumer(RoomContainer); 
+
+
+
+
